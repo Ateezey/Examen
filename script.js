8cbaec5573b4b861920f16cb599640c1b8601e00
Unknown <aterzitane@gmail.com>
2018-04-06 10:53:29 +0200

    changes

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ $(window).scroll( function(){
     var bottom_of_window = $(window).scrollTop() + $(window).height();
     if( bottom_of_window > bottom_of_object ){       
         $(this).animate({'opacity':'1'},600);    
+        console.log('test');
       }
 	}); 
  }); 
+});
+$('#scroll-to-register').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
 });
