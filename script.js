35781d1575e3e99ebff2edc0592f87b8788cb5db
Unknown <aterzitane@gmail.com>
2018-04-27 17:54:59 +0200

    Modified things

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ $(window).scroll(function () {
     var bottom_of_window = $(window).scrollTop() + $(window).height();
     if (bottom_of_window > bottom_of_object) {
       $(this).animate({ 'opacity': '1' }, 600);
-
     }
   });
 });
+
 
 
 function smoothScroll() {// Function to smooth all anchors
