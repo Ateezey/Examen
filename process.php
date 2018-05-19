1819de3e67962154b6149ead6f3fd4574d3a63ec
Unknown <aterzitane@gmail.com>
2018-05-19 14:40:07 +0200

    changes

--- process.php
+++ process.php
@@ -39,7 +39,9 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<div class="blablabla"><br> id: ". $row["id"]. " - Name: ". $row["name"]. " " . $row["lastname"] . " - Email: " . $row["email"] . "<br></div>";
+		echo"<div class="blablabla">";
+		echo "<br> Name: ". $row["name"]. " " . $row["lastname"] . " - Email: " . $row["email"] . "<br>";
+		echo "<div>";
     }
 } else {
     echo "0 results";
