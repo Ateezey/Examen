ad20df9ebf56d55cf3abadd227e382527a695de9
Unknown <aterzitane@gmail.com>
2018-05-20 16:28:42 +0200

    changes

--- index.php
+++ index.php
@@ -52,13 +52,16 @@ if (!$db) {
   <div class="container">
     <div class="row justify-content-center" id="read-more">
       <div class="col-lg-6 single-text-box test">
-        <h1 class="text-center">Header</h1>
-        <p>Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet,
-          när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem
-          ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar.
-          Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum , och
-          senare med mjukvaror som Aldus PageMaker.
-        </p>
+        <h1 class="text-center">
+        <?php
+							$query = "SELECT * FROM content WHERE ID = 2";
+    						$result = mysqli_query($db, $query);
+    						$ord = mysqli_fetch_assoc($result);
+    						echo $ord['header']; 
+							?></h2>
+							<p><?php
+   							echo $ord['breadtext'];
+   							?></p>
       </div>
     </div>
   </div>
